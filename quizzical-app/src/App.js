bd9e7eafd5d833f4d42f1828f7c1e4e62c5ed8c9
ccheng3 <ccrcheng@ucdavis.edu>
2022-05-01 21:10:11 -0700

    added fetch() call to API and set the questions to state array variable

--- quizzical-app/src/App.js
+++ quizzical-app/src/App.js
@@ -2,7 +2,30 @@ import React from 'react';
 import StartPage from './StartPage';
 import TriviaPage from './TriviaPage';
 
+// https://opentdb.com/api.php?amount=5&difficulty=medium&type=multiple
+
 export default function App() {
+   // save the trivia questions in state to be changed upon new game
+   const [triviaQuestions, setTriviaQuestions] = React.useState([]);
+
+   // fetch the trivia questions data once during each game
+   React.useEffect(() => {
+      async function getQuestions() {
+         const res = await fetch('https://opentdb.com/api.php?amount=5&difficulty=medium&type=multiple');
+         const data = await res.json();
+         setTriviaQuestions(data.results);
+      };
+      getQuestions();
+   }, []);
+
+   function printQuestions() {
+      triviaQuestions.forEach(question => {
+         console.log(question.question);
+      })
+   }
+   printQuestions();
+
+
    return (
       <section>
          <StartPage />
