bdf05f635f2de45b6987c5fc8339d7092060ca25
ccheng3 <ccrcheng@ucdavis.edu>
2022-05-01 19:03:15 -0700

    first commit of Scrimba Beginner React Solo Project

---
+++ quizzical-app/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import StartPage from './StartPage';
+import TriviaPage from './TriviaPage';
+
+export default function App() {
+   return (
+      <section>
+         <StartPage />
+      </section>
+   );
+}
---
+++ quizzical-app/src/StartPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+// This component is the Quizzical start page. 
+// H2 that says 'Quizzical'
+// Div with description --> 'Let's do some trivia!'
+// Btn element that says 'Start quiz'
+
+export default function StartPage() {
+   return (
+      <main>
+         <h2 className='startPage-title'>Quizzical</h2>
+         <div className='startPage-description'>Let's do some trivia! (Drinks not provided but def encouraged!)</div>
+         <div className='startPage-btn' backgroundcolor='red'>Start quiz</div>
+      </main>
+   )
+}
